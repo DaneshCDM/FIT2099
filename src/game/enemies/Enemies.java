436f49b7020f97ad13ff2831ecdca33314fd73cc
muhammadabdullahakif <maki0001@student.monash.edu>
2022-04-26 02:29:44 +0800

    Added a behaviours package in the game package

--- src/game/enemies/Enemies.java
+++ src/game/enemies/Enemies.java
@@ -7,6 +7,10 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.*;
+import game.behaviours.AttackBehaviour;
+import game.behaviours.Behaviour;
+import game.behaviours.FollowBehaviour;
+import game.behaviours.WanderBehaviour;
 
 import java.util.HashMap;
 import java.util.Map;
