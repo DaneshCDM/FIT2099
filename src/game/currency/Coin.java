6bb1e1dda47dfc0a3be54d01f30db70ff858b216
dmar0038 <dmar0038@student.monash.edu>
2022-04-30 04:24:04 +0800

    Req5: Added Coin Class

---
+++ src/game/currency/Coin.java
@@ -0,0 +1,55 @@
+package game.currency;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.Location;
+
+/**
+ *  * Coin class manages everything to do with a Coin Object
+ *  *
+ *  * @author Danesh Mariapan
+ */
+public class Coin extends Item {
+
+    private static final String NAME = "Coin";
+    private static final Character DISPLAY_CHARACTER = '$';
+    private static final boolean PORTABLE = true;
+    private int value;
+
+    /**
+     * Coin Constructor
+     * @param value the currency value that the Coin has
+     */
+    public Coin(int value){
+        super(NAME, DISPLAY_CHARACTER, PORTABLE);
+        this.value = value;
+    }
+
+    // Getter for Coin Value
+    public int getValue() {
+        return value;
+    }
+
+    // Setter for Coin Value
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void tick(Location currentLocation, Actor actor) {
+        this.tick(currentLocation);
+    }
+
+    // Returns "Coin ($value)
+    @Override
+    public String toString() {
+        return super.toString() + " " + "(" + this.getDisplayChar() + this.getValue() + ")";
+    }
+
+    // Returns the type of Coin ($value)
+    public String getCoinType(){
+        return this.toString();
+    }
+
+
+}
