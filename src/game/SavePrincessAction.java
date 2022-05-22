28f4277511dfc9c333815e7b2b14e6f7c8cce565
dmar0038 <dmar0038@student.monash.edu>
2022-05-22 18:08:51 +0800

    Added SavePrincessAction - To save Princess for End Game

---
+++ src/game/SavePrincessAction.java
@@ -0,0 +1,44 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.magicalitems.Key;
+
+import static game.enemies.Bowser.bowserKey;
+
+public class SavePrincessAction extends Action {
+
+    private final Actor target;
+
+    /**
+     * Constructor for the Action
+     * @param target the Actor that we want to interact with (Princess Peach)
+     */
+    public SavePrincessAction(Actor target) {
+        this.target = target;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+
+        // Check if Player/Mario has the Key to save Princess Peach
+        if (actor.getInventory().contains(bowserKey)){
+            return ("*******************************************" + "\n" +
+                    "***       CONGRATULATIONS PLAYER!       ***" + "\n" +
+                    "*******************************************" + "\n" +
+                    "***     You saved Princess Peach :D     ***" + "\n" +
+                    "*******************************************");
+        }
+        else{
+            return (target) + ": " + "Mario you need to Kill Bowser and take his Key to save me! ;( ";
+        }
+
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "Save Princess Peach";
+    }
+
+}
